235185fb2855dad409a5758009552176c03f7158
YellowFlash1040 <andrejkovtunec@gmail.com>
2026-02-04 12:44:11 +0100

    v5

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 set -e
 
-wp='php84 -d memory_limit=256M /usr/local/bin/wp'
-
 # Download WordPress if not present
 if [ ! -d "${WORDPRESS_DATA}/wp-admin" ]; then
-    ${wp} core download --allow-root --locale=en_US --path=${WORDPRESS_DATA}
+    wp core download --allow-root --locale=en_US --path=${WORDPRESS_DATA}
     chown -R ${WORDPRESS_USER}:${WORDPRESS_USER} ${WORDPRESS_DATA}
 fi
 
@@ -16,7 +14,7 @@ done
 
 # Create config if it doesn't exist
 if [ ! -f "${WORDPRESS_DATA}/wp-config.php" ]; then
-    ${wp} config create --allow-root \
+    wp config create --allow-root \
         --dbname=${WP_DATABASE} \
         --dbuser=${WP_ADMIN_USER} \
         --dbpass=${WP_PASSWORD} \
@@ -25,9 +23,8 @@ if [ ! -f "${WORDPRESS_DATA}/wp-config.php" ]; then
 fi
 
 # Install WordPress if not already installed
-if ! ${wp} core is-installed --allow-root --path=${WORDPRESS_DATA} 2>/dev/null; then
-    echo "Installing WordPress..."
-    ${wp} core install --allow-root \
+if ! wp core is-installed --allow-root --path=${WORDPRESS_DATA} 2>/dev/null; then
+    wp core install --allow-root \
         --url=${WP_URL} \
         --title="${WP_TITLE}" \
         --admin_user=${WP_ADMIN_USER} \
@@ -37,9 +34,8 @@ if ! ${wp} core is-installed --allow-root --path=${WORDPRESS_DATA} 2>/dev/null; 
 fi
 
 # Create user if it doesn't exist
-if ! ${wp} user get "${WP_USER}" --allow-root --path=${WORDPRESS_DATA} 2>/dev/null; then
-    echo "Creating user ${WP_USER}..."
-    ${wp} user create --allow-root \
+if ! wp user get "${WP_USER}" --allow-root --path=${WORDPRESS_DATA} 2>/dev/null; then
+    wp user create --allow-root \
         "${WP_USER}" "${WP_EMAIL}" \
         --user_pass="${WP_PASSWORD}" \
         --role=author \
